0c4a3513cfe94b09e4e10694408131e7e3ccb398
Gabriel Luiz <dev.gabrielluiz@gmail.com>
2023-03-09 20:17:03 -0300

    First commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import './index.css';
+
+function App() {
+  return (
+    <>
+      <h1>Hello world!</h1>
+      <h2>Reverse Pomodoro WIP</h2>
+    </>
+  );
+}
+
+export default App;
