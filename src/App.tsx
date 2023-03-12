cf5d62361e1b9e6b3cd024b2e44806a9e9cc962d
Gabriel Luiz <dev.gabrielluiz@gmail.com>
2023-03-12 18:51:29 -0300

    Added favicon and logo + title

--- src/App.tsx
+++ src/App.tsx
@@ -4,10 +4,19 @@ import { MainTimer } from './components/MainTimer';
 import { Footer } from './components/Footer';
 
 import './index.scss';
+import reversePomodoroImg from './images/pomodoro.png';
 
 function App() {
   return (
     <>
+      <div id="logo">
+        <img
+          className="logoImg"
+          src={reversePomodoroImg}
+          alt="Reverse Pomodoro logo"
+        />
+        <h1>Reverse Pomodoro</h1>
+      </div>
       <MainTimer
         chillTime={25 * 60}
         shortWorkTime={5 * 60}
