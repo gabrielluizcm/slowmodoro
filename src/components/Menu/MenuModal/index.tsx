20118e359e6b0f01d1a5fbcdae96c8d8dd17a51a
Gabriel Luiz <dev.gabrielluiz@gmail.com>
2023-03-28 20:44:16 -0300

    -Added buttons to menu drawer -Added info modal
    Note: will probably change scss for styled components before continuing

--- src/components/Menu/MenuModal/index.tsx
+++ src/components/Menu/MenuModal/index.tsx
@@ -4,21 +4,19 @@ import { FaMinus } from 'react-icons/fa';
 import './style.scss';
 
 type MenuModalProps = {
+  children: JSX.Element;
   closeModal: () => void;
 };
 
 export function MenuModal(props: MenuModalProps) {
   return (
     <>
-      <div id="backShadow" onClick={props.closeModal} />
-      <div id="content">
-        <div id="closeModal" onClick={props.closeModal}>
+      <div className="backShadow" onClick={props.closeModal} />
+      <div className="content">
+        <div className="closeModal" onClick={props.closeModal}>
           <FaMinus />
         </div>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi,
-        veritatis? Molestias, at accusantium dolor unde repellat laboriosam
-        reiciendis necessitatibus ad sint sequi quos nulla, excepturi, minus
-        quis quo doloremque provident!
+        {props.children}
       </div>
     </>
   );
--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { FaBars } from 'react-icons/fa';
+import { FaBars, FaCog, FaInfo, FaLink } from 'react-icons/fa';
 
 import { MenuModal } from './MenuModal';
 
@@ -8,6 +8,7 @@ import './style.scss';
 export function Menu() {
   const [opened, setOpened] = useState(false);
   const [menuClass, setMenuClass] = useState('');
+  const [modalInfoOpen, setModalInfoOpen] = useState(false);
 
   const handleIconClick = () => {
     setMenuClass(opened ? 'closing' : 'opening');
@@ -16,13 +17,54 @@ export function Menu() {
     setTimeout(() => setMenuClass(newOpened ? 'opened' : ''), 300);
   };
 
+  const handleCogClick = () => {
+    handleIconClick();
+    setModalInfoOpen(true);
+  };
+
   return (
     <>
-      <div id="menuBackground" className={menuClass}>
+      <div id="menuIcon">
         <FaBars onClick={handleIconClick} />
       </div>
+      <div id="menuBackground" className={menuClass}>
+        <></>
+        <FaCog onClick={handleCogClick} />
+        <FaInfo />
+      </div>
 
-      {/* {opened ? <MenuModal closeModal={() => setOpened(false)} /> : false} */}
+      {modalInfoOpen ? (
+        <MenuModal closeModal={() => setModalInfoOpen(false)}>
+          <>
+            <a
+              href="https://en.wikipedia.org/wiki/Pomodoro_Technique"
+              title="Pomodoro Technique page on Wikipedia"
+            >
+              <FaLink />
+              What is the Pomodoro Technique?
+            </a>
+            <hr />
+            <a
+              href="https://www.tiimoapp.com/blog/reverse-pomodoro-technique/"
+              title="Reverse Pomodoro Article on TiimoApp"
+            >
+              <FaLink />
+              About the Reverse Pomodoro Technique
+            </a>
+            <span className="copyright">
+              <a
+                href="https://www.flaticon.com/free-icons/pomodoro"
+                title="Pomodoro Icons on Flaticon"
+              >
+                <FaLink />
+                Pomodoro icon created by Freepik - Flaticon using online editor
+              </a>
+            </span>
+          </>
+        </MenuModal>
+      ) : (
+        false
+      )}
     </>
   );
 }
