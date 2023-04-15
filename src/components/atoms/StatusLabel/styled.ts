c419114bb922fdd5e9d66238cd152b2e7afcafc4
Gabriel Luiz <dev.gabrielluiz@gmail.com>
2023-04-15 14:48:46 -0300

    Decreased vertical margins to better fit on smaller screens

--- src/components/atoms/StatusLabel/styled.ts
+++ src/components/atoms/StatusLabel/styled.ts
@@ -2,7 +2,7 @@ import styled from 'styled-components';
 
 const StyledStatus = styled.h1`
   text-align: center;
-  margin: 1em 0;
+  margin: 20px 0;
 `;
 
 export { StyledStatus };
--- src/components/atoms/Timer/styled.ts
+++ src/components/atoms/Timer/styled.ts
@@ -3,7 +3,7 @@ import styled from 'styled-components';
 const StyledTimer = styled.div`
   font-size: 4em;
   text-align: center;
-  margin: 1em 0;
+  margin: 20px 0;
 `;
 
 export { StyledTimer };
